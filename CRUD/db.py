bc851f94f16f87eb8d16b8a1e4a1e41b2d84a591
Love  Louidor <105249391+Louidor-Love@users.noreply.github.com>
2024-09-18 23:33:16 +0000

    nuevo

--- CRUD/db.py
+++ CRUD/db.py
@@ -1,10 +1,67 @@
 from dotenv import load_dotenv
+from sqlmodel import Field, Session, SQLModel, create_engine, select,delete
 import os
 
 load_dotenv('.env')
 
 # sqlite_file_name = "database.db"
-sqlite_url = os.getenv('postgresql://ecommerce_db_n48b_user:ljLooAWfGpzLP20RTsb6tWfea9cnXNkk@dpg-crd2lbt2ng1s73fndht0-a.oregon-postgres.render.com/ecommerce_db_n48b')
+sqlite_url = os.getenv('DATABASE_URL')
 
 # connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, echo=True)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+class Task(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    title: str = Field(index=True)
+    description: str
+    img_url: str = ''
+
+
+class Users(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    first_name: str
+    last_name: str
+    email: str
+    password: str
+
+
+
+def readAllTasks(search:str):
+    with Session(engine) as session:
+        tasks = []
+        if(search != None):
+           tasks = session.exec(select(Task)).all()
+        if(search != ''):
+            tasks = session.exec(select(Task).where(Task.title.like(f'%{search}%'))).all()        
+        return tasks
+
+def createTask(newTask:Task):
+    with Session(engine) as session:
+        session.add(newTask)
+        session.commit()
+        session.refresh(newTask)
+        return newTask
+
+def deleteTask(id:int):
+    with Session(engine) as session:
+        task = session.exec(delete(Task).where(Task.id == id))
+        session.commit()
+        return task
+
+
+
+def authUser(attemptEmail, attemptPassword):
+    # hace query, si encuentra una fila, devuelve userId. Si no, devuelve False
+    """ select id from users
+        where email = 'bruno@test.com'
+        AND password = 'bruno234'        
+    """
+    with Session(engine) as session:
+        userId = session.exec(  select(Users.id).where(Users.email == attemptEmail).where(Users.password == attemptPassword)  ).first()
+        print('authUser userId',  userId)
+        return userId
+    
+    ...
--- CRUD/main.py
+++ CRUD/main.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI, Request,Response,Form
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse ,RedirectResponse
 from sqlmodel import Field
 from typing import Annotated
 
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from db import Task, create_db_and_tables, engine, readAllTasks, createTask, deleteTask, authUser
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+def root(req: Request):
+  return templates.TemplateResponse("login.html", {"request": req})
+
+
+
+
+
+
+
+
+
